2f6b8db0e6ac39f87d030079488e530457696130
jesselangdon <jesselangdon@gmail.com>
2022-12-13 13:50:51 -0800

    Preparations and The Basics chapters

---
+++ chpTheBasics/ex2.js
@@ -0,0 +1,4 @@
+console.log((4936 - 936) / 1000); // returns 4
+console.log((4936 - 4036) / 100); // returns 9
+console.log((4936 % 4900) % 33); // returns 3
+console.log(4936 % 4930);
---
+++ chpTheBasics/ex4.js
@@ -0,0 +1,2 @@
+/* The 10 is implicitly coerced into a string data type, and concatenated
+with the '5' string literal */
